cce31a3e4eb9a0c0598391c6c05c66a10f824a58
chinzhiwei <chinzhiweiblank@gmail.com>
2021-11-04 01:08:07 +0800

    Remove negative calculation of x coord

--- question2.m
+++ question2.m
@@ -1,23 +1,27 @@
+% Focal length in pixels
 F=961;
+ 
+% Eight corresponding points of two images
 pts1 = [[129 322]; [544 1127]; [225 133]; [335 168]; [380 341]; [210 738]; [210 748]; [224 738];];
 pts2 = [[304 318]; [725 1106]; [390 114]; [499 137]; [544 309]; [385 708]; [386 717]; [399 708];];
+
 % Center coordinates of image
 X0 = 960/2;
 Y0 = 1280/2;
 
 % First image
 X = [pts1 ones(8,1)*F];
-X (: ,1) = -( X (: ,1) - X0 ) ;
+X (: ,1) = X (: ,1) - X0 ;
 X (: ,2) = X (: ,2) - Y0 ;
 
 % Second image
 P = [pts2 ones(8,1)*F];
-P (: ,1) = -( P (: ,1) - X0 ) ;
+P (: ,1) = P (: ,1) - X0 ;
 P (: ,2) = P (: ,2) - Y0 ;
 
 % Obtain the L2 norm along each row
 X_norm = vecnorm (X , 2 , 2) ;
-% Row - wise devision of vector with it ’s norm
+% Row - wise devision of vector with its norm
 X = X ./ X_norm ;
 
 % Likewise for P
@@ -35,4 +39,6 @@ end
 [U , S , V ] = svd( W ) ;
 R = U * transpose ( V ) ;
 angle = acos((trace(R)-1)/2);
-rad2deg(angle)
+rad2deg(angle);
+
+% angle = 10.2375 degrees;
